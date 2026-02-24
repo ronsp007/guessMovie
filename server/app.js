ec09ea45aad1e2296b025a8563cb21f2c8068a9a
Ronja <ronjals007@gmail.com>
2026-02-24 16:42:26 +0100

    Added the complete function in the server for getting all the relevant information ready for transfere to client.

--- server/app.js
+++ server/app.js
@@ -53,9 +53,11 @@ const server = http.createServer((req,res) => {
 
 });
 
+
 server.listen(port, hostname, () => {
     console.log("The server is running an listening at: " + serverUrl);
 });
+
 
 //standardfunction for sending respons to client
 function sendRespons(res, statusCode, contentType, data){
@@ -91,33 +93,111 @@ async function test(res, search){
 }
 
 
-//Asks database for 10 random entry titles(ever one uniqe)
-//Imports the 10 correspondning pictures. 
-//For each of the main 10 movies, 5 other random movie names (which are different from the main movie) needs to be includeed, these will be the answer options
-async function routingPictureGame(res) {
-    const tenMovieQuestion = await randomMovies(10, null);
+// so instead of doing this we can manipulated the database and run a code seperatly that:
+// checks if the movie in the database has a picture
+// adds a tag to the movies that don't have a picture
+// When serching for the random movies exclude movies with said tag
+async function checkImageAndReplace(movie){
+    
+    let test = null; 
+    let imageData = null; 
+    const limit = 100; 
+    let n = 0;
+
+    while(test == null && n < limit){
+
+        const imageFilePath = "./media" + movie.normalized_id + ".png";
+
+        try{
+
+            imageData = await fs.readFile(imageFilePath);
+            test = 1;
+            return imageData;
+
+        }catch(err){
+            n++; 
+            console.log(movie + " does not have picture");
+            const newMovie = await randomMovies(1, movie);
+            movie = newMovie[0];
+
+        }
+
+    }
+    if(n == 100){
+        throw new Error("Error fetching data");
+    }else{
+        return imageData;
+    }
+}
+
+//Asks database for numr random entry titles(every one distinct)
+//Imports the numr correspondning pictures. 
+//For each of the main numr movies, diff other random movie names (which are different from the main movie) needs to be includeed: these will be the answer options
+async function routingPictureGame(res, numr, diff) {
+    
+    const resultToClient = {
+        QuestionMovie: null,
+        answerOptionsForQuestions: null, 
+        QuestionPicture: null,
+    };
+
+
+    const tenMovieQuestion = await randomMovies(numr, null);
+    resultToClient.QuestionMovie = tenMovieQuestion;
+
+
+    let imagePaths = [];
+
+    for(let i = 0; i < numr; i++){
+        //this will be replaced
+        const imageData = await checkImageAndReplace(QuestionMovie[i]);
+        imagePaths.push(imageData);
+
+    }
+    resultToClient.QuestionPicture = imagePaths;
+
+    let answerOptions = []; //empty array that will store the answer options for each movie
+    for(let i = 0; i < numr; i++){
+        const fiveMovies = await randomMovies(diff, QuestionMovie[i]);
+        answerOptions.push(fiveMovies);
+    }
+    resultToClient.answerOptionsForQuestions = answerOptions;
+
+
+   
+
+    const stringToClient = JSON.stringify(resultToClient);
+    sendRespons(res, 200, "application/json", stringToClient);
 
 }
 
-//asks database for numr amount for randomized movies that are distinct from check. 
-async function randomMovies(res, numr, check) {
+//asks database for numr amount of randomized movies that are distinct from check. 
+async function randomMovies(numr, check) {
 
-    if(check != null){ //if no paremeter is inclueded
-        
-        await dbClient.connect();
-        const db = dbClient.db("tnm121-project");
-        const  dbCollection = db.collection("imdb");
+    await dbClient.connect();
+    const db = dbClient.db("tnm121-project");
+    const  dbCollection = db.collection("imdb");
+    
 
-        const findResult = await dbCollection.aggregate([{$sample: {size: numr}}]).toArray();
+    if(check == null){ //if no perimiter is inclueded
+
+        const sampelFilter = [{$sample: {size: numr}}];
+        const findResult = await dbCollection.aggregate(sampelFilter).toArray();
         console.log(findResult);
-        const resultJson = {Questions: findResult};
-        const findResultString = JSON.stringify(resultJson);
 
-        sendRespons(res,200,"application/json",findResultString);
         await dbClient.close();
+        return findResult
         
     }else{
-        console.log("Not yet implemented");
+        const sampelFilter = [
+            {$sample: {size: numr}}, //sets how many entries we want
+            {$match: {normalized_id: {$ne: check.normalized_id}}} //sets witch entry we want to exclude
+        ];
+
+        const findResult = await dbCollection.aggregate(sampelFilter).toArray();
+        
+        await dbClient.close();
+        return findResult; 
     }
 
 }
